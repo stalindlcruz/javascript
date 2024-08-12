159f9ff14c47c408959387099c8274363f61d5a4
Engels Valdez <engels.valdezcastillo@gmail.com>
2024-08-11 22:09:33 -0700

    class today number tree

---
+++ 03/scripts.js
@@ -0,0 +1,60 @@
+// let person = {
+//     name: 'Stalin',
+//     age: 28,
+//     hobby: ['pay video games', 'watch tv'],
+//     chapters: {
+//         one: 'any',
+//         sencond: 'any',
+//         three: {
+//             one: [{one:23},['qloq', {any:'qqqqqqqqq', place: 'any'}]]
+//         }
+//     }
+// }
+
+// console.log(person.chapters.three.one[1][1].place);
+
+
+// let lista = [['1','2',3], true, {}]
+
+// lista[0][2]
+
+
+// let fruistList = [ 'banana', 'apple', 'orange' ];
+
+// function getFruistList(position) {
+//     if (position == 0) {
+//         return fruistList[position];
+//     }
+// }
+
+// let result = getFruistList(0)
+
+// console.log(result);
+
+// let getFruistList = function () {
+//     return fruistList
+// }
+
+
+// let getFruistList = () => {
+//     return fruistList;
+// }
+
+// let getFruistList = () =>fruistList;
+
+// console.log(getFruistList());
+
+// let btnI = document.querySelector('#btn');
+
+// let printMsjInConsole = () => console.log('hola');
+
+// btnI.addEventListener('click', printMsjInConsole)
+
+// let printEnvent = () => {
+//    console.log('HEllo');
+   
+// }
+
+// btnI.addEventListener('click', printEnvent)
+
+// btnI.addEventListener('mouseenter', printEnvent)
