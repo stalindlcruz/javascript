17ff36608eb1b270bb5072f28a51440cff5fcaca
stalindlcruz <stalindlcruz@gmail.com>
2024-08-20 14:42:32 -0400

    second commit II

---
+++ 03/practicing-03.js
@@ -0,0 +1,90 @@
+// let car = {
+//     plate: 'NH209167',
+//     model: 'Tesla',
+//     features: [
+//         'Air Conditioning',
+//         'Cruise Control',
+//         'Leather seats',
+//         'Reverse Camera',
+//     ],
+//     priceByColor: {
+//         white: '$48,000',
+//         black: '$49,999',
+//         red: '$54,950',
+//         Blue: {
+//             darBlue: '$56,749',
+//             lightBlue: '$59,999',
+//         }
+//     }
+// }
+
+// console.log(car.priceByColor.Blue.lightBlue);
+// console.log(car.features[2]);
+
+
+
+// let lista = [
+//     ['1', '2', '3'],
+//     true,
+//     {}
+// ]
+
+// console.log(lista[0][1]);
+
+// let fruitList = ['Apple', 'Peach', 'Blueberry']
+
+
+// function printMessage (msj) {
+//     console.log(msj);  
+// }
+
+// printMessage('Holaaa')
+
+
+// function getMessage(msj) {
+//     return msj
+// }
+
+// let message = getMessage('Keloque')
+
+// console.log(message);
+
+
+
+// let fruitList = ['Apple', 'Peach', 'Blueberry']
+
+// function getFruitlist(position) {
+//     if (position == 0) {
+//         console.log('Buena eleccion')
+//         return fruitList[position]
+//     }
+//     else {
+//         console.log('Mala desiscion');
+//         return 'Hola'
+//     }
+    
+// }
+
+// let result = getFruitlist(0)
+
+// console.log(result);
+
+
+
+// let getFruitlist = function() {
+//     return getFruitlist;
+// }
+
+// let getFruitlist = () => {
+//     return getFruitlist;
+// }
+
+// let getFruistList = () =>fruitList;
+
+// console.log(getFruitlist());
+
+
+
+let btn1 = document.querySelector('#btn');
+
+let printMessage = () => console.log('Hola');
