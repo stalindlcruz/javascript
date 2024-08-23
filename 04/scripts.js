c53b879a796901ccdc699bbad5d819f3c02f9bcc
Engels Valdez <engels.valdezcastillo@gmail.com>
2024-08-22 22:27:39 -0700

    hola

---
+++ 04/scripts.js
@@ -0,0 +1,94 @@
+
+
+// Loops => bucles
+
+//   for
+//   while
+//   do while
+
+// let fruits = ['Orange', 'Banana', 'Apple', 'Kiwi', 'Peach', 'Bluebery']
+// let count = 0;
+
+// while(count <= 10) {
+//     // count = count + 1
+//     // count += 1;
+//     count++
+//     console.log(fruits[count]);
+// }
+
+// do {
+//     count++
+//     console.log(fruits[count]);
+// } while (count <= 10)
+
+// let thename;
+
+// do {
+//     thename = prompt('cual es tu nomnbre');
+// } while (thename !== 'stalin')
+
+// for( let i = 0; i < 10; i++ ) {
+//     console.log(fruits[i]);
+// }
+
+////////////////\
+
+// let select = document.querySelector('#fruits');
+
+// for( let i = 0; i < fruits.length; i++ ) {
+//     let options = document.createElement('option');
+//     options.text = fruits[i];
+//     select.appendChild(options);
+// }
+
+////////////////////
+
+// let print = (funct) => {
+//     let r = funct()
+//     console.log(r);
+//     console.log(funct);
+// }
+
+// print(function(){ return 1 })
+
+// let print = (funct) => {
+//    let r = funct();
+//    console.log(r);
+   
+// }
+
+// let play = (func) => {
+//     return true
+// }
+
+// print(play)
+
+// let print = (funct, p) => {
+//     let r = funct(p);
+//     console.log(r);
+    
+//  }
+ 
+//  let play = (p) => {
+//      return p
+//  }
+ 
+//  print(play, false)
+
+// let myForEach = (callback)=>{
+//     for(let i= 0; i < fruits.length; i++){
+//         let r = callback(fruits[i])
+//         console.log(r);
+//     }
+// }
+
+// let myCallback = (fruit) => fruit;
+// myForEach(myCallback)
+
+/////////////////// Bonus
+
+// (function () {
+//     console.log('Me invoke sola!');
+// })()
+
+
