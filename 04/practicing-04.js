e2f44ee12e8157dd1cc0999493f474c303b7670d
stalindlcruz <stalindlcruz@gmail.com>
2024-08-24 14:04:44 -0400

    Practicing class 04

---
+++ 04/practicing-04.js
@@ -0,0 +1,36 @@
+/* 
+    loop and calback
+    -----------------
+    • while
+    • do while
+    • for
+*/
+
+
+/* let carList = ['Tesla', 'Honda', 'Subaru', 'Toyota', 'Mercedez']
+let count = 0; */
+
+/* while (count <= 3) {
+    console.log(carList[count]);
+    // count = count + 1;
+    // count += 1;
+    count++;
+} */
+
+
+/* do {
+    console.log(carList[count]);
+    count++;
+} while (count <= 5); */
+
+
+/* let thename;
+
+do {
+    thename = prompt('Cual es tu nombre?');
+} while (thename !== 'stalin'); */
+
+
+/* for (let count = 0; count < 2; count++) {
+    console.log(carList[count]);
+} */
