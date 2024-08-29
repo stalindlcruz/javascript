2e3012ba7ab8243189d59787e9f4efb4d16a1be4
stalindlcruz <stalindlcruz@gmail.com>
2024-08-29 00:27:31 -0400

    practicing callback's class

--- 05/practicing-05.js
+++ 05/practicing-05.js
@@ -8,3 +8,30 @@ mondayExercise('the monday exercise is run') */
 
 
 
+/* function cooking(mainMeal) {
+    mainMeal('The rice is the main meal!'); 
+}
+
+function cookingRice(message) {
+    console.log(message);
+}
+
+cooking(cookingRice) */
+
+
+
+/* let playingBaseball = (firstBaseman, shortStop) => {
+    console.log('Las posiciones First Baseman y Short Stop, estaran ocupadas por Engels y Stalin')
+    firstBaseman('Engels esta jugando la primera base')
+    shortStop('Stalin esta jugando en el short stop')
+}
+
+let firstBaseman = (engels) => {
+    console.log(engels);
+}
+
+let shortStop = (stalin) => {
+    console.log(stalin);
+}
+
+playingBaseball(firstBaseman, shortStop); */
