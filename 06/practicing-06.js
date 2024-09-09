1e54cd00f32eecbb009a4b02e04504c7385d89bb
stalindlcruz <stalindlcruz@gmail.com>
2024-09-09 15:13:34 -0400

    studying class 06 partTwo

--- 06/practicing-06.js
+++ 06/practicing-06.js
@@ -1,7 +1,57 @@
-let cadena = new String();
+/* let cadena = new String();
 let saludo = 'Hola';
 
-new Boolean();
-let boolean = true;
 
-console.log(cadena);
+new Boolean();
+let Boolean = true;
+
+
+new Array();
+let lista = []
+
+
+console.log(cadena); */
+
+
+
+/* function Car(brand, model) {
+    this.brand = brand;
+    this.model = model;
+    this.displayInfo = function () {
+        console.log(`Car: ${this.brand} ${this.model}`);
+
+    };
+};
+
+let myCar = new Car ('Toyota', 'Corolla');
+
+myCar.displayInfo(); */
+
+
+/* ------------------------------------------------------------------------------------------------ */
+
+
+
+/* let nombre = new String();
+
+let result = nombre.concat('Stalin ', 'De La Cruz');
+console.log(result); */
+
+
+/* let firstName = 'Engels';
+let lastName = 'Valdez';
+
+const fullName = firstName.concat(' ', lastName);
+
+console.log(fullName); */
+
+
+let carBrands = {
+    Tesla: 'Model-X',
+    Honda: 'CRV',
+    Toyota: 'Hihglander',
+}
+
+const models = Object.values(carBrands);
+
+console.log(models);
