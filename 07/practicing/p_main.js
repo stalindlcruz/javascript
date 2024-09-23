64d5a6e4b809a5820b4e38dc9f7924a4722e0640
stalindlcruz <stalindlcruz@gmail.com>
2024-09-23 14:34:25 -0400

    practcing class #7

---
+++ 07/practicing/p_main.js
@@ -0,0 +1,52 @@
+/* LocalStorage */
+
+// const person = {
+//     name: 'Engels',
+//     age: 35
+// }
+
+// localStorage.setItem("Name", "Engels");
+// localStorage.setItem("Person", person);
+// localStorage.setItem("User", "user1");
+
+// const personData = JSON.stringify(person);
+// localStorage.setItem("Person", personData);
+
+// localStorage.removeItem("User");
+
+// let getData = localStorage.getItem("Person");
+// const convertData = JSON.parse(getData);
+// console.log(convertData.age);
+
+// localStorage.clear();
+
+/* ---------------------------------------------------------- */
+
+/* sessionStorage */
+
+// const emails = {
+//     Engels: 'engels@gmail.com',
+//     Stalin: 'stalin@gmail.com',
+// }
+
+// sessionStorage.setItem('userEmail', 'mail@email.com')
+
+// let emailsToString = JSON.stringify(emails);
+// sessionStorage.setItem('Emails', emailsToString);
+
+
+// let getEmails = sessionStorage.getItem('Emails');
+// let stringToParce = JSON.parse(getEmails);
+// console.log(stringToParce);
+
+/* ---------------------------------------------------------- */
+
+/* Cookies */
+
+// document.cookie = 'Name=Engels Valdez';
+document.cookie = 'Name21=Stalin De La Cruz; expires=Mon, sep 23 2024 14:33:00 GMT-4';
+// document.cookie = "username=John Doe; expires=Thu, 18 Dec 2026 12:00:00 UTC";
+
+// date = new Date().getTime();
+// console.log(date);
+
