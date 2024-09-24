a9fefc6aae1edffed9b66a6308ac8167349cba2c
stalindlcruz <stalindlcruz@gmail.com>
2024-09-24 01:07:27 -0400

    practcing class #7 part II

--- 07/practicing/p_main.js
+++ 07/practicing/p_main.js
@@ -44,9 +44,37 @@
 /* Cookies */
 
 // document.cookie = 'Name=Engels Valdez';
-document.cookie = 'Name21=Stalin De La Cruz; expires=Mon, sep 23 2024 14:33:00 GMT-4';
+// document.cookie = 'Name21=Stalin De La Cruz; expires=Mon, sep 23 2025 14:33:00 GMT-4';
 // document.cookie = "username=John Doe; expires=Thu, 18 Dec 2026 12:00:00 UTC";
 
 // date = new Date().getTime();
 // console.log(date);
 
+// document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";  <-- Para indicar a que ruta pertenece sin el path la cookie pertenece a la pagina actual.
+
+
+// let readCookie = document.cookie;
+
+// console.log(readCookie);
+
+
+// document.cookie = 'Name21=Stalin De La Cruz; expires=Mon, sep 23 2025 14:33:00 UTC; path=/';
+
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', ()=>{
+    const valueCookie = {
+        name: 'Stalin',
+        age: 20,
+    }
+
+    document.cookie = `username=${JSON.stringify(valueCookie)}; expires=Thu, 30 Sep 2025 12:00:00 UTC;`;
+
+})
+
+let deco = document.cookie.split('=');
+let data = JSON.parse(deco[1])
+
+console.log(document.cookie);
+// console.log(deco);
+// console.log(data);
