0d41c10b055dd96a8c68a3270ab32a5ab69fe2ed
stalindlcruz <stalindlcruz@gmail.com>
2024-09-26 01:22:12 -0400

    funciones para agregar, actualizar y eliminar cookies (completed)

---
+++ 07/practicing/practice/func-cookies.js
@@ -0,0 +1,25 @@
+/* Create three functions to work with cookies: ( add, update and delete).
+    The add function must receive three parameters:
+        1. field name
+        2. value and expiration date
+        3. the last one should be a default value
+            (The default value is tomorrow's date). */
+
+
+
+const addingCookie = (name, value, date = 'Fri, 27 Sep 2024 12:00:00 UTC') => {
+    document.cookie = `${name}=${value}; expires=${date}; path=/`;
+}
+
+const updatingCookie = (name, value, date = 'Fri, 27 Dec 2024 12:00:00 UTC') => {
+    deletingCookie(name);
+    addingCookie(name, value, date);
+}
+
+const deletingCookie = (name) => {
+    document.cookie = `${name}=; expires=Mon, 10 Jan 1920 12:00:00 UTC; path=/`;
+}
+
+addingCookie('USER1', 'Dahiana Brens', 'Fri, 30 Sep 2024 12:00:00 UTC');
+updatingCookie('USER2', 'Florangel Moya', 'Fri, 30 Sep 2024 12:00:00 UTC');
+deletingCookie('USER1');
