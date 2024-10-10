8ddd8b61f1f911af2b5d539fc5036a0ad6d394ce
Engels Valdez <engels.valdezcastillo@gmail.com>
2024-10-10 07:57:06 -0700

    homework08

---
+++ 08/scripts.js
@@ -0,0 +1,5 @@
+// Homework
+
+// Hacer una pantalla web que contenga un teclado numerico y una barra encima de los numero como si fuese una 
+// calculadar "pero no es una calculadora, ya que solo sera presentar cada numero presionado", que al presinar cada tecla "numero" se vaya reflejando en 
+// la barra, y que contenga dos botones extra, uno para borrar el ultimo numero presionado y otro para borrarlo todo.
