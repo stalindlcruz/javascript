a8ed85183d3783208c04bd1aea52e59d036ef866
Engels Valdez <engels.valdezcastillo@gmail.com>
2024-11-17 22:25:43 -0800

    the best class today

---
+++ 11/scripts.js
@@ -0,0 +1,55 @@
+
+// put / path
+// get 
+// post
+// delete
+
+// CRUD 
+
+// ENDPOINT
+
+ 
+// async function getUser() {
+//     try {
+//         const result = await fetch('https://randomuser.me/api/',{ method: 'get' });
+//         const convertToJson = result.json();
+//         return convertToJson
+        
+//     } catch (error) {
+//         throw error;
+//     }
+// }
+
+// getUser().then((data)=>{
+//     const name = document.querySelector('#Name');
+//     const r = data.results[0];
+//     console.log(r);
+//     name.innerHTML = r.name.first
+// })
+
+// https://developer.mozilla.org/es/docs/Web/HTTP/Status
+
+// entrypoint = punto de entrada
+// endpoint = punto final
+
+ 
+// async function getUser() {
+//     try {
+//         const result = await fetch('https://jsonplaceholder.typicode.com/posts/3',{ 
+//             method: 'post', 
+//             body : {  },
+//             headers: []
+//         });
+//         const convertToJson = result.json();
+//         return convertToJson
+        
+//     } catch (error) {
+//         throw error;
+//     }
+// }
+
+// getUser().then((data)=>{
+
+//     const r = data
+//     console.log(r);
+// })
