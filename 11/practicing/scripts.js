7f24651e0ec98d12283707e665052b7401d7b21c
Engels Valdez <engels.valdezcastillo@gmail.com>
2024-12-11 19:11:54 -0800

    xa

--- 11/practicing/scripts.js
+++ 11/practicing/scripts.js
@@ -16,17 +16,22 @@ async function fetchRandomUser() {
     try {
         const response = await fetch('https://randomuser.me/api/');
         const data = await response.json();
-        const userPicture = data.results[0].picture.large;
-        const firstName = data.results[0].name.first;
-        const lastName = data.results[0].name.last;
-        // const name = data.results[0].name.first + ' ' + data.results[0].name.last;
-        const name = `${firstName} ${lastName}`
+        const { name, picture } = data.results[0];
+        // const user = data.results[0];
+        // const userPicture = data.results[0].picture.large;
+        // const firstName = data.results[0].name.first;
+        // const lastName = data.results[0].name.last;
+        // // const name = data.results[0].name.first + ' ' + data.results[0].name.last;
+        // const name = `${firstName} ${lastName}`
+        // const fullName = `${user.name.first} ${user.name.last}`
+        const fullName = `${name.first} ${name.last}`;
 
-        userName.innerHTML = name;
-        userImage.src = userPicture;
+        userName.innerHTML = fullName;
+        userImage.src = picture.large;
     } catch (error) {
         console.error('Error fetching random user:', error);
     }
 }
 
 fetchUserBtn.addEventListener('click', fetchRandomUser);
+window.addEventListener('load',fetchRandomUser)
