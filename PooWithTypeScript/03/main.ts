0d06f99f5a1c611ae0375d369b4a514764031675
stalindlcruz <stalindlcruz@gmail.com>
2025-01-14 23:51:01 -0500

    class 03 ENCAPSULACION

---
+++ PooWithTypeScript/03/main.ts
@@ -0,0 +1,77 @@
+// class Persona {
+
+//     constructor(public nombre: string) {}
+
+//     public correr(x:any): void {
+      
+//     }
+
+// }
+
+// const persona = new Persona('Stalin');
+
+// console.log(persona.nombre)
+
+// class Login {
+//     private user: string = 'admin';
+//     private pass: string = '1234';
+
+//     singIn(user: string, pass: string) {
+//         if(user === this.user && pass === this.pass) {
+//             console.log('Success')
+//         } else {
+//             console.log('Failed')
+//         }
+//     }
+
+//     // getter
+//     // setter
+
+//     // public setUser(value: string) {
+//     //     this.user = value;
+//     // }
+
+//     // public getUser() {
+//     //     return this.getUser
+//     // }
+
+//     set _user(value: string) {
+//         this.user = value;
+//     }
+
+//     get _user() {
+//         return this.user
+//     }
+// }
+
+// const login = new Login();
+
+// login._user = 'Engels';
+
+// console.log(login._user)
+// // login.setUser('juan')
+
+
+// login.singIn('juan','1234')
+
+// class Person {
+
+//     private static nombre: string
+//      constructor() {}
+
+//      public x: any;
+
+//      public static getNombre() {
+//         return this.nombre;
+//      }
+
+//     public static getNombre() {
+//         return ''
+//      }
+// }
+
+// const persona = new Person('Stalin');
+
+// console.log(persona.getNombre())
+
+// console.log(Person.getNombre())
