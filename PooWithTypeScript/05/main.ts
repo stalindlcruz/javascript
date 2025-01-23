a07d8a035ba2bad4ab1a7b2e2c63caf39055536d
stalindlcruz <stalindlcruz@gmail.com>
2025-01-22 20:31:41 -0500

    abstracion

---
+++ PooWithTypeScript/05/main.ts
@@ -0,0 +1,5 @@
+/* Abstracion */
+
+// https://matiashernandez.dev/blog/post/types-vs-interfaces-cuando-utilizar-cada-una
+// https://codigolinea.com/evite-usar-any-use-unknown-en-typescript/
+// https://www.typescriptlang.org/docs/handbook/utility-types.html
